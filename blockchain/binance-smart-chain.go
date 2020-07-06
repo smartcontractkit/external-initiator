a30c0438c6cc5fc6e1ea0c8c52b3a2a5ea4d0ae3
boxhock <jonas@smartcontract.com>
2020-07-06 11:26:37 +0200

    Re-use functionality from ETH integration

--- blockchain/binance-smart-chain.go
+++ blockchain/binance-smart-chain.go
@@ -2,7 +2,6 @@ package blockchain
 
 import (
 	"encoding/json"
-	"errors"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/common/hexutil"
 	"github.com/smartcontractkit/chainlink/core/eth"
@@ -18,8 +17,7 @@ const BSC = "binance-smart-chain"
 // The bscManager implements the subscriber.JsonManager interface and allows
 // for interacting with ETH nodes over RPC or WS.
 type bscManager struct {
-	fq *bscFilterQuery
-	p  subscriber.Type
+	ethManager
 }
 
 // createBscManager creates a new instance of bscManager with the provided
@@ -30,17 +28,21 @@ func createBscManager(p subscriber.Type, config store.Subscription) bscManager {
 		addresses = append(addresses, common.HexToAddress(a))
 	}
 
+	// Hard-set the topics to match the OracleRequest()
+	// event emitted by the oracle contract provided.
 	topics := [][]common.Hash{{
 		models.RunLogTopic20190207withoutIndexes,
 		common.HexToHash(StringToBytes32(config.Job)),
 	}}
 
 	return bscManager{
-		fq: &bscFilterQuery{
-			Addresses: addresses,
-			Topics:    topics,
+		ethManager{
+			fq: &filterQuery{
+				Addresses: addresses,
+				Topics:    topics,
+			},
+			p: p,
 		},
-		p: p,
 	}
 }
 
@@ -53,40 +55,7 @@ func createBscManager(p subscriber.Type, config store.Subscription) bscManager {
 // If bscManager is using RPC:
 // Sends a "eth_getLogs" request.
 func (e bscManager) GetTriggerJson() []byte {
-	if e.p == subscriber.RPC && e.fq.FromBlock == "" {
-		e.fq.FromBlock = "latest"
-	}
-
-	filter, err := e.fq.toMapInterface()
-	if err != nil {
-		return nil
-	}
-
-	filterBytes, err := json.Marshal(filter)
-	if err != nil {
-		return nil
-	}
-
-	msg := jsonrpcMessage{
-		Version: "2.0",
-		ID:      json.RawMessage(`1`),
-	}
-
-	switch e.p {
-	case subscriber.WS:
-		msg.Method = "eth_subscribe"
-		msg.Params = json.RawMessage(`["logs",` + string(filterBytes) + `]`)
-	case subscriber.RPC:
-		msg.Method = "eth_getLogs"
-		msg.Params = json.RawMessage(`[` + string(filterBytes) + `]`)
-	}
-
-	bytes, err := json.Marshal(msg)
-	if err != nil {
-		return nil
-	}
-
-	return bytes
+	return e.ethManager.GetTriggerJson()
 }
 
 // GetTestJson generates a JSON payload to test
@@ -98,22 +67,7 @@ func (e bscManager) GetTriggerJson() []byte {
 // If bscManager is using RPC:
 // Sends a request to get the latest block number.
 func (e bscManager) GetTestJson() []byte {
-	if e.p == subscriber.RPC {
-		msg := jsonrpcMessage{
-			Version: "2.0",
-			ID:      json.RawMessage(`1`),
-			Method:  "eth_blockNumber",
-		}
-
-		bytes, err := json.Marshal(msg)
-		if err != nil {
-			return nil
-		}
-
-		return bytes
-	}
-
-	return nil
+	return e.ethManager.GetTestJson()
 }
 
 // ParseTestResponse parses the response from the
@@ -127,24 +81,7 @@ func (e bscManager) GetTestJson() []byte {
 // Attempts to parse the block number in the response.
 // If successful, stores the block number in bscManager.
 func (e bscManager) ParseTestResponse(data []byte) error {
-	if e.p == subscriber.RPC {
-		var msg jsonrpcMessage
-		if err := json.Unmarshal(data, &msg); err != nil {
-			return err
-		}
-		var res string
-		if err := json.Unmarshal(msg.Result, &res); err != nil {
-			return err
-		}
-		e.fq.FromBlock = res
-	}
-
-	return nil
-}
-
-type bscSubscribeResponse struct {
-	Subscription string          `json:"subscription"`
-	Result       json.RawMessage `json:"result"`
+	return e.ethManager.ParseTestResponse(data)
 }
 
 // ParseResponse parses the response from the
@@ -155,7 +92,7 @@ type bscSubscribeResponse struct {
 // If there are new events, update bscManager with
 // the latest block number it sees.
 func (e bscManager) ParseResponse(data []byte) ([]subscriber.Event, bool) {
-	logger.Debugw("Parsing response", "ExpectsMock", ExpectsMock)
+	logger.Debugw("Parsing Binance Smart Chain response", "ExpectsMock", ExpectsMock)
 
 	var msg jsonrpcMessage
 	if err := json.Unmarshal(data, &msg); err != nil {
@@ -167,7 +104,7 @@ func (e bscManager) ParseResponse(data []byte) ([]subscriber.Event, bool) {
 
 	switch e.p {
 	case subscriber.WS:
-		var res bscSubscribeResponse
+		var res ethSubscribeResponse
 		if err := json.Unmarshal(msg.Params, &res); err != nil {
 			logger.Error("unmarshal:", err)
 			return nil, false
@@ -200,7 +137,7 @@ func (e bscManager) ParseResponse(data []byte) ([]subscriber.Event, bool) {
 	case subscriber.RPC:
 		var rawEvents []eth.Log
 		if err := json.Unmarshal(msg.Result, &rawEvents); err != nil {
-			logger.Error(err)
+			logger.Error("unmarshal:", err)
 			return nil, false
 		}
 
@@ -230,7 +167,7 @@ func (e bscManager) ParseResponse(data []byte) ([]subscriber.Event, bool) {
 
 			fromBlkn, err := hexutil.DecodeBig(e.fq.FromBlock)
 			if err != nil && !(e.fq.FromBlock == "latest" || e.fq.FromBlock == "") {
-				logger.Error(err)
+				logger.Error("Failed to get block number from event:", err)
 				continue
 			}
 
@@ -243,49 +180,4 @@ func (e bscManager) ParseResponse(data []byte) ([]subscriber.Event, bool) {
 	}
 
 	return events, true
-}
-
-type bscFilterQuery struct {
-	BlockHash *common.Hash     // used by eth_getLogs, return logs only from block with this hash
-	FromBlock string           // beginning of the queried range, nil means genesis block
-	ToBlock   string           // end of the range, nil means latest block
-	Addresses []common.Address // restricts matches to events created by specific contracts
-
-	// The Topic list restricts matches to particular event topics. Each event has a list
-	// of topics. Topics matches a prefix of that list. An empty element slice matches any
-	// topic. Non-empty elements represent an alternative that matches any of the
-	// contained topics.
-	//
-	// Examples:
-	// {} or nil          matches any topic list
-	// {{A}}              matches topic A in first position
-	// {{}, {B}}          matches any topic in first position AND B in second position
-	// {{A}, {B}}         matches topic A in first position AND B in second position
-	// {{A, B}, {C, D}}   matches topic (A OR B) in first position AND (C OR D) in second position
-	Topics [][]common.Hash
-}
-
-func (q bscFilterQuery) toMapInterface() (interface{}, error) {
-	arg := map[string]interface{}{
-		"address": q.Addresses,
-		"topics":  q.Topics,
-	}
-	if q.BlockHash != nil {
-		arg["blockHash"] = *q.BlockHash
-		if q.FromBlock != "" || q.ToBlock != "" {
-			return nil, errors.New("cannot specify both BlockHash and FromBlock/ToBlock")
-		}
-	} else {
-		if q.FromBlock == "" {
-			arg["fromBlock"] = "0x0"
-		} else {
-			arg["fromBlock"] = q.FromBlock
-		}
-		if q.ToBlock == "" {
-			arg["toBlock"] = "latest"
-		} else {
-			arg["toBlock"] = q.ToBlock
-		}
-	}
-	return arg, nil
 }
--- blockchain/binance-smart-chain_test.go
+++ blockchain/binance-smart-chain_test.go
@@ -1,16 +1,12 @@
 package blockchain
 
 import (
-	"math/big"
 	"reflect"
 	"testing"
 
 	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/common/hexutil"
-	"github.com/magiconair/properties/assert"
 	"github.com/smartcontractkit/external-initiator/store"
 	"github.com/smartcontractkit/external-initiator/subscriber"
-	"github.com/stretchr/testify/require"
 )
 
 func TestCreateBscFilterMessage(t *testing.T) {
@@ -49,64 +45,16 @@ func TestCreateBscFilterMessage(t *testing.T) {
 
 	t.Run("has invalid filter query", func(t *testing.T) {
 		blockHash := common.HexToHash("0xabc")
-		got := bscManager{fq: &bscFilterQuery{BlockHash: &blockHash, FromBlock: "0x1", ToBlock: "0x2"}}.GetTriggerJson()
+		got := bscManager{ethManager{fq: &filterQuery{BlockHash: &blockHash, FromBlock: "0x1", ToBlock: "0x2"}}}.GetTriggerJson()
 		if got != nil {
 			t.Errorf("GetTriggerJson() = %s, want nil", got)
 		}
 	})
 }
 
-func Test_bscToFilterArg(t *testing.T) {
-	type args struct {
-		q bscFilterQuery
-	}
-
-	blockHash := common.HexToHash("abc")
-
-	tests := []struct {
-		name    string
-		args    args
-		wantErr bool
-	}{
-		{
-			"cannot specify both Blockhash and FromBlock",
-			args{bscFilterQuery{
-				BlockHash: &blockHash,
-				FromBlock: hexutil.EncodeBig(big.NewInt(3234512922)),
-			}},
-			true,
-		},
-		{
-			"cannot specify both Blockhash and ToBlock",
-			args{bscFilterQuery{
-				BlockHash: &blockHash,
-				ToBlock:   hexutil.EncodeBig(big.NewInt(3234512922)),
-			}},
-			true,
-		},
-		{
-			"regular query passes",
-			args{bscFilterQuery{
-				Addresses: []common.Address{},
-				Topics:    [][]common.Hash{},
-			}},
-			false,
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			_, err := tt.args.q.toMapInterface()
-			if (err != nil) != tt.wantErr {
-				t.Errorf("toFilterArg() error = %v, wantErr %v", err, tt.wantErr)
-				return
-			}
-		})
-	}
-}
-
 func TestBscManager_GetTestJson(t *testing.T) {
 	type fields struct {
-		fq *bscFilterQuery
+		fq *filterQuery
 		p  subscriber.Type
 	}
 	tests := []struct {
@@ -132,8 +80,10 @@ func TestBscManager_GetTestJson(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			e := bscManager{
-				fq: tt.fields.fq,
-				p:  tt.fields.p,
+				ethManager{
+					fq: tt.fields.fq,
+					p:  tt.fields.p,
+				},
 			}
 			if got := e.GetTestJson(); !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("GetTestJson() = %v, want %v", got, tt.want)
@@ -144,7 +94,7 @@ func TestBscManager_GetTestJson(t *testing.T) {
 
 func TestBscManager_ParseTestResponse(t *testing.T) {
 	type fields struct {
-		fq *bscFilterQuery
+		fq *filterQuery
 		p  subscriber.Type
 	}
 	type args struct {
@@ -159,28 +109,28 @@ func TestBscManager_ParseTestResponse(t *testing.T) {
 	}{
 		{
 			"does nothing for WS",
-			fields{fq: &bscFilterQuery{}, p: subscriber.WS},
+			fields{fq: &filterQuery{}, p: subscriber.WS},
 			args{},
 			false,
 			"",
 		},
 		{
 			"parses RPC responses",
-			fields{fq: &bscFilterQuery{}, p: subscriber.RPC},
+			fields{fq: &filterQuery{}, p: subscriber.RPC},
 			args{[]byte(`{"jsonrpc":"2.0","id":1,"result":"0x1"}`)},
 			false,
 			"0x1",
 		},
 		{
 			"fails unmarshal payload",
-			fields{fq: &bscFilterQuery{}, p: subscriber.RPC},
+			fields{fq: &filterQuery{}, p: subscriber.RPC},
 			args{[]byte(`error`)},
 			true,
 			"",
 		},
 		{
 			"fails unmarshal result",
-			fields{fq: &bscFilterQuery{}, p: subscriber.RPC},
+			fields{fq: &filterQuery{}, p: subscriber.RPC},
 			args{[]byte(`{"jsonrpc":"2.0","id":1,"result":["0x1"]}`)},
 			true,
 			"",
@@ -189,8 +139,10 @@ func TestBscManager_ParseTestResponse(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			e := bscManager{
-				fq: tt.fields.fq,
-				p:  tt.fields.p,
+				ethManager{
+					fq: tt.fields.fq,
+					p:  tt.fields.p,
+				},
 			}
 			if err := e.ParseTestResponse(tt.args.data); (err != nil) != tt.wantErr {
 				t.Errorf("ParseTestResponse() error = %v, wantErr %v", err, tt.wantErr)
@@ -203,9 +155,12 @@ func TestBscManager_ParseTestResponse(t *testing.T) {
 }
 
 func TestBscManager_ParseResponse(t *testing.T) {
-	type fields struct {
-		fq *bscFilterQuery
+	type manager struct {
+		fq *filterQuery
 		p  subscriber.Type
+	}
+	type fields struct {
+		manager
 	}
 	type args struct {
 		data []byte
@@ -220,7 +175,7 @@ func TestBscManager_ParseResponse(t *testing.T) {
 	}{
 		{
 			"fails parsing invalid payload",
-			fields{fq: &bscFilterQuery{}, p: subscriber.WS},
+			fields{manager{fq: &filterQuery{}, p: subscriber.WS}},
 			args{data: []byte(`invalid`)},
 			nil,
 			false,
@@ -228,7 +183,7 @@ func TestBscManager_ParseResponse(t *testing.T) {
 		},
 		{
 			"fails parsing invalid WS subscribe payload",
-			fields{fq: &bscFilterQuery{}, p: subscriber.WS},
+			fields{manager{fq: &filterQuery{}, p: subscriber.WS}},
 			args{data: []byte(`{"jsonrpc":"2.0","id":1,"params":[]}`)},
 			nil,
 			false,
@@ -236,7 +191,7 @@ func TestBscManager_ParseResponse(t *testing.T) {
 		},
 		{
 			"fails parsing invalid WS subscribe",
-			fields{fq: &bscFilterQuery{}, p: subscriber.WS},
+			fields{manager{fq: &filterQuery{}, p: subscriber.WS}},
 			args{data: []byte(`{"jsonrpc":"2.0","id":1,"params":{"subscription":"test","result":[]}}`)},
 			nil,
 			false,
@@ -244,7 +199,7 @@ func TestBscManager_ParseResponse(t *testing.T) {
 		},
 		{
 			"successfully parses WS Oracle request",
-			fields{fq: &bscFilterQuery{}, p: subscriber.WS},
+			fields{manager{fq: &filterQuery{}, p: subscriber.WS}},
 			args{data: []byte(`{"jsonrpc":"2.0","id":1,"params":{"subscription":"test","result":{"data":"0x0000000000000000000000007d0965224facd7156df0c9a1adf3a94118026eeb354f99e2ac319d0d1ff8975c41c72bf347fb69a4874e2641bd19c32e09eb88b80000000000000000000000000000000000000000000000000de0b6b3a76400000000000000000000000000007d0965224facd7156df0c9a1adf3a94118026eeb92cdaaf300000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000005ef1cd6b00000000000000000000000000000000000000000000000000000000000000010000000000000000000000000000000000000000000000000000000000000100000000000000000000000000000000000000000000000000000000000000005663676574783f68747470733a2f2f6d696e2d6170692e63727970746f636f6d706172652e636f6d2f646174612f70726963653f6673796d3d455448267473796d733d5553446470617468635553446574696d65731864","address":"0xFadfF79bA04F169386646a43869B66B39c7E0858","logIndex":"0x0","blockNumber":"0x2","blockHash":"0xabc0000000000000000000000000000000000000000000000000000000000000","transactionHash":"0xabc0000000000000000000000000000000000000000000000000000000000000","transactionIndex":"0x0","topics":["0xd8d7ecc4800d25fa53ce0372f13a416d98907a7ef3d8d3bdd79cf4fe75529c65","0x0000000000000000000000000000000000000000000000000000000000000000"]}}}`)},
 			[]subscriber.Event{subscriber.Event(`{"address":"0xFadfF79bA04F169386646a43869B66B39c7E0858","dataPrefix":"0x354f99e2ac319d0d1ff8975c41c72bf347fb69a4874e2641bd19c32e09eb88b80000000000000000000000000000000000000000000000000de0b6b3a76400000000000000000000000000007d0965224facd7156df0c9a1adf3a94118026eeb92cdaaf300000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000005ef1cd6b","functionSelector":"0x4ab0d190","get":"https://min-api.cryptocompare.com/data/price?fsym=ETH\u0026tsyms=USD","path":"USD","times":100}`)},
 			true,
@@ -252,7 +207,7 @@ func TestBscManager_ParseResponse(t *testing.T) {
 		},
 		{
 			"fails parsing invalid RPC payload",
-			fields{fq: &bscFilterQuery{}, p: subscriber.RPC},
+			fields{manager{fq: &filterQuery{}, p: subscriber.RPC}},
 			args{data: []byte(`{"jsonrpc":"2.0","id":1,"result":{}}`)},
 			nil,
 			false,
@@ -260,7 +215,7 @@ func TestBscManager_ParseResponse(t *testing.T) {
 		},
 		{
 			"fails parsing invalid block number in RPC event payload",
-			fields{fq: &bscFilterQuery{}, p: subscriber.RPC},
+			fields{manager{fq: &filterQuery{}, p: subscriber.RPC}},
 			args{data: []byte(`{"jsonrpc":"2.0","id":1,"result":[{"data":"0x0000000000000000000000007d0965224facd7156df0c9a1adf3a94118026eeb354f99e2ac319d0d1ff8975c41c72bf347fb69a4874e2641bd19c32e09eb88b80000000000000000000000000000000000000000000000000de0b6b3a76400000000000000000000000000007d0965224facd7156df0c9a1adf3a94118026eeb92cdaaf300000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000005ef1cd6b00000000000000000000000000000000000000000000000000000000000000010000000000000000000000000000000000000000000000000000000000000100000000000000000000000000000000000000000000000000000000000000005663676574783f68747470733a2f2f6d696e2d6170692e63727970746f636f6d706172652e636f6d2f646174612f70726963653f6673796d3d455448267473796d733d5553446470617468635553446574696d65731864","address":"0xFadfF79bA04F169386646a43869B66B39c7E0858","logIndex":"0x0","blockNumber":"abc","blockHash":"0xabc0000000000000000000000000000000000000000000000000000000000000","transactionHash":"0xabc0000000000000000000000000000000000000000000000000000000000000","transactionIndex":"0x0","topics":["0xd8d7ecc4800d25fa53ce0372f13a416d98907a7ef3d8d3bdd79cf4fe75529c65","0x0000000000000000000000000000000000000000000000000000000000000000"]}]}`)},
 			nil,
 			false,
@@ -268,7 +223,7 @@ func TestBscManager_ParseResponse(t *testing.T) {
 		},
 		{
 			"updates fromBlock from RPC payload",
-			fields{fq: &bscFilterQuery{}, p: subscriber.RPC},
+			fields{manager{fq: &filterQuery{}, p: subscriber.RPC}},
 			args{data: []byte(`{"jsonrpc":"2.0","id":1,"result":[{"data":"0x0000000000000000000000007d0965224facd7156df0c9a1adf3a94118026eeb354f99e2ac319d0d1ff8975c41c72bf347fb69a4874e2641bd19c32e09eb88b80000000000000000000000000000000000000000000000000de0b6b3a76400000000000000000000000000007d0965224facd7156df0c9a1adf3a94118026eeb92cdaaf300000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000005ef1cd6b00000000000000000000000000000000000000000000000000000000000000010000000000000000000000000000000000000000000000000000000000000100000000000000000000000000000000000000000000000000000000000000005663676574783f68747470733a2f2f6d696e2d6170692e63727970746f636f6d706172652e636f6d2f646174612f70726963653f6673796d3d455448267473796d733d5553446470617468635553446574696d65731864","address":"0xFadfF79bA04F169386646a43869B66B39c7E0858","logIndex":"0x0","blockNumber":"0x3","blockHash":"0xabc0000000000000000000000000000000000000000000000000000000000000","transactionHash":"0xabc0000000000000000000000000000000000000000000000000000000000000","transactionIndex":"0x0","topics":["0xd8d7ecc4800d25fa53ce0372f13a416d98907a7ef3d8d3bdd79cf4fe75529c65","0x0000000000000000000000000000000000000000000000000000000000000000"]}]}`)},
 			[]subscriber.Event{subscriber.Event(`{"address":"0xFadfF79bA04F169386646a43869B66B39c7E0858","dataPrefix":"0x354f99e2ac319d0d1ff8975c41c72bf347fb69a4874e2641bd19c32e09eb88b80000000000000000000000000000000000000000000000000de0b6b3a76400000000000000000000000000007d0965224facd7156df0c9a1adf3a94118026eeb92cdaaf300000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000005ef1cd6b","functionSelector":"0x4ab0d190","get":"https://min-api.cryptocompare.com/data/price?fsym=ETH\u0026tsyms=USD","path":"USD","times":100}`)},
 			true,
@@ -276,7 +231,7 @@ func TestBscManager_ParseResponse(t *testing.T) {
 		},
 		{
 			"does not update fromBlock in the past from RPC payload",
-			fields{fq: &bscFilterQuery{FromBlock: "0x1"}, p: subscriber.RPC},
+			fields{manager{fq: &filterQuery{FromBlock: "0x1"}, p: subscriber.RPC}},
 			args{data: []byte(`{"jsonrpc":"2.0","id":1,"result":[{"data":"0x0000000000000000000000007d0965224facd7156df0c9a1adf3a94118026eeb354f99e2ac319d0d1ff8975c41c72bf347fb69a4874e2641bd19c32e09eb88b80000000000000000000000000000000000000000000000000de0b6b3a76400000000000000000000000000007d0965224facd7156df0c9a1adf3a94118026eeb92cdaaf300000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000005ef1cd6b00000000000000000000000000000000000000000000000000000000000000010000000000000000000000000000000000000000000000000000000000000100000000000000000000000000000000000000000000000000000000000000005663676574783f68747470733a2f2f6d696e2d6170692e63727970746f636f6d706172652e636f6d2f646174612f70726963653f6673796d3d455448267473796d733d5553446470617468635553446574696d65731864","address":"0xFadfF79bA04F169386646a43869B66B39c7E0858","logIndex":"0x0","blockNumber":"0x0","blockHash":"0xabc0000000000000000000000000000000000000000000000000000000000000","transactionHash":"0xabc0000000000000000000000000000000000000000000000000000000000000","transactionIndex":"0x0","topics":["0xd8d7ecc4800d25fa53ce0372f13a416d98907a7ef3d8d3bdd79cf4fe75529c65","0x0000000000000000000000000000000000000000000000000000000000000000"]}]}`)},
 			[]subscriber.Event{subscriber.Event(`{"address":"0xFadfF79bA04F169386646a43869B66B39c7E0858","dataPrefix":"0x354f99e2ac319d0d1ff8975c41c72bf347fb69a4874e2641bd19c32e09eb88b80000000000000000000000000000000000000000000000000de0b6b3a76400000000000000000000000000007d0965224facd7156df0c9a1adf3a94118026eeb92cdaaf300000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000005ef1cd6b","functionSelector":"0x4ab0d190","get":"https://min-api.cryptocompare.com/data/price?fsym=ETH\u0026tsyms=USD","path":"USD","times":100}`)},
 			true,
@@ -286,8 +241,10 @@ func TestBscManager_ParseResponse(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			e := bscManager{
-				fq: tt.fields.fq,
-				p:  tt.fields.p,
+				ethManager{
+					fq: tt.fields.fq,
+					p:  tt.fields.p,
+				},
 			}
 			got, got1 := e.ParseResponse(tt.args.data)
 			if !reflect.DeepEqual(got, tt.want) {
@@ -298,89 +255,6 @@ func TestBscManager_ParseResponse(t *testing.T) {
 			}
 			if e.fq.FromBlock != tt.expectedFromBlock {
 				t.Errorf("FromBlock = %s, expected %s", e.fq.FromBlock, tt.expectedFromBlock)
-			}
-		})
-	}
-}
-
-func Test_bscFilterQuery_toMapInterface(t *testing.T) {
-	type fields struct {
-		BlockHash *common.Hash
-		FromBlock string
-		ToBlock   string
-		Addresses []common.Address
-		Topics    [][]common.Hash
-	}
-	tests := []struct {
-		name    string
-		fields  fields
-		want    map[string]interface{}
-		wantErr bool
-	}{
-		{
-			"empty toBlock becomes latest",
-			fields{},
-			map[string]interface{}{
-				"address":   []common.Address{},
-				"topics":    [][]common.Hash{},
-				"fromBlock": "0x0",
-				"toBlock":   "latest",
-			},
-			false,
-		},
-		{
-			"uses non-empty toBlock",
-			fields{ToBlock: "0x1"},
-			map[string]interface{}{
-				"address":   []common.Address{},
-				"topics":    [][]common.Hash{},
-				"fromBlock": "0x0",
-				"toBlock":   "0x1",
-			},
-			false,
-		},
-		{
-			"empty fromBlock becomes 0x0",
-			fields{},
-			map[string]interface{}{
-				"address":   []common.Address{},
-				"topics":    [][]common.Hash{},
-				"fromBlock": "0x0",
-				"toBlock":   "latest",
-			},
-			false,
-		},
-		{
-			"uses non-empty fromBlock",
-			fields{FromBlock: "0x1"},
-			map[string]interface{}{
-				"address":   []common.Address{},
-				"topics":    [][]common.Hash{},
-				"fromBlock": "0x1",
-				"toBlock":   "latest",
-			},
-			false,
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			q := bscFilterQuery{
-				BlockHash: tt.fields.BlockHash,
-				FromBlock: tt.fields.FromBlock,
-				ToBlock:   tt.fields.ToBlock,
-				Addresses: tt.fields.Addresses,
-				Topics:    tt.fields.Topics,
-			}
-			got, err := q.toMapInterface()
-			if (err != nil) != tt.wantErr {
-				t.Errorf("toMapInterface() error = %v, wantErr %v", err, tt.wantErr)
-				return
-			}
-			mapInterface, ok := got.(map[string]interface{})
-			require.True(t, ok)
-			keys := []string{"fromBlock", "toBlock"}
-			for _, key := range keys {
-				assert.Equal(t, mapInterface[key], tt.want[key])
 			}
 		})
 	}
