c27a435566005b15f69e993538aacd461bf3b1cf
Kristijan Rebernisak <kristijan.rebernisak@gmail.com>
2020-07-12 00:16:47 +0200

    Improve scripts TypeScript sub-project
        - Add and improve TS type definitions
        - Add eslint & prettier configuration

--- integration/scripts/src/createJob.ts
+++ integration/scripts/src/createJob.ts
@@ -1,52 +1,53 @@
-// @ts-ignore
 import url from 'url'
-import {getArgs, getLoginCookie, registerPromiseHandler,} from './common'
+import axios from 'axios'
 
-const axios = require('axios')
+import { getArgs, getLoginCookie, registerPromiseHandler } from './common'
 
 async function main() {
-    registerPromiseHandler();
-    const args = getArgs(['CHAINLINK_URL']);
+  registerPromiseHandler()
+  const args = getArgs(['CHAINLINK_URL'])
 
-    await createJob({
-        chainlinkUrl: args.CHAINLINK_URL,
-    })
+  await createJob({
+    chainlinkUrl: args.CHAINLINK_URL,
+  })
 }
 
-main();
+main()
 
 interface Options {
-    chainlinkUrl: string
+  chainlinkUrl: string
 }
 
-async function createJob({chainlinkUrl}: Options) {
-    const sessionsUrl = url.resolve(chainlinkUrl, '/sessions');
-    const job = {
-        initiators: [
-            {
-                type: 'external',
-                params: {
-                    name: "mock-client",
-                    body: {
-                        endpoint: process.argv[2],
-                        addresses: [process.argv[3]],
-                        accountIds: [process.argv[4]],
-                    }
-                }
-            }
-        ],
-        tasks: [{type: 'noop'}]
-    };
-    const specsUrl = url.resolve(chainlinkUrl, '/v2/specs');
-    const Job = await axios.post(specsUrl, job, {
-        withCredentials: true,
-        headers: {
-            cookie: await getLoginCookie(sessionsUrl)
-        }
-    }).catch((e: any) => {
-        console.error(e);
-        throw Error(`Error creating Job ${e}`)
-    });
+async function createJob({ chainlinkUrl }: Options) {
+  const sessionsUrl = url.resolve(chainlinkUrl, '/sessions')
+  const job = {
+    initiators: [
+      {
+        type: 'external',
+        params: {
+          name: 'mock-client',
+          body: {
+            endpoint: process.argv[2],
+            addresses: [process.argv[3]],
+            accountIds: [process.argv[4]],
+          },
+        },
+      },
+    ],
+    tasks: [{ type: 'noop' }],
+  }
+  const specsUrl = url.resolve(chainlinkUrl, '/v2/specs')
+  const Job = await axios
+    .post(specsUrl, job, {
+      withCredentials: true,
+      headers: {
+        cookie: await getLoginCookie(sessionsUrl),
+      },
+    })
+    .catch((e: Error) => {
+      console.error(e)
+      throw Error(`Error creating Job ${e}`)
+    })
 
-    console.log('Deployed Job at:', Job.data.data.id);
+  console.log('Deployed Job at:', Job.data.data.id)
 }
