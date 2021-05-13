9a00d4f4dd6e6ce2a8ae620f02e590da3ec5b5f3
aalu1418 <aalu1418@gmail.com>
2021-05-13 14:46:07 -0400

    added test cases to klaytn mock-client

--- integration/mock-client/blockchain/klaytn_test.go
+++ integration/mock-client/blockchain/klaytn_test.go
@@ -7,6 +7,85 @@ import (
 	"testing"
 )
 
+func getKlaytnLogResponse(address string, topics []string) klaytnLogResponse {
+	return klaytnLogResponse{
+		LogIndex:         "0x0",
+		BlockNumber:      "0x2",
+		BlockHash:        "0xabc0000000000000000000000000000000000000000000000000000000000000",
+		TransactionHash:  "0xabc0000000000000000000000000000000000000000000000000000000000000",
+		TransactionIndex: "0x0",
+		Address:          address,
+		Data:             "0x0000000000000000000000007d0965224facd7156df0c9a1adf3a94118026eeb354f99e2ac319d0d1ff8975c41c72bf347fb69a4874e2641bd19c32e09eb88b80000000000000000000000000000000000000000000000000de0b6b3a76400000000000000000000000000007d0965224facd7156df0c9a1adf3a94118026eeb92cdaaf300000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000005ef1cd6b00000000000000000000000000000000000000000000000000000000000000010000000000000000000000000000000000000000000000000000000000000100000000000000000000000000000000000000000000000000000000000000005663676574783f68747470733a2f2f6d696e2d6170692e63727970746f636f6d706172652e636f6d2f646174612f70726963653f6673796d3d455448267473796d733d5553446470617468635553446574696d65731864",
+		Topics:           topics,
+	}
+}
+
+func Test_klaytnLogRequestToResponse(t *testing.T) {
+	type args struct {
+		msg JsonrpcMessage
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    klaytnLogResponse
+		wantErr bool
+	}{
+		{
+			"correct eth_log request",
+			args{
+				JsonrpcMessage{
+					Params: json.RawMessage(fmt.Sprintf(`[{"topics":[["%s"]],"address":["%s"]}]`, hash.String(), address.String())),
+				},
+			},
+			getKlaytnLogResponse(address.String(), []string{hash.String()}),
+			false,
+		},
+		{
+			"eth_log request with empty topics",
+			args{
+				JsonrpcMessage{
+					Params: json.RawMessage(fmt.Sprintf(`[{"topics":[null],"address":["%s"]}]`, address.String())),
+				},
+			},
+			getKlaytnLogResponse(address.String(), nil),
+			false,
+		},
+		{
+			"misformed payload",
+			args{
+				JsonrpcMessage{
+					Params: json.RawMessage(`[{"topics":[null],"address":"0x0"}]`),
+				},
+			},
+			klaytnLogResponse{},
+			true,
+		},
+		{
+			"empty request",
+			args{
+				JsonrpcMessage{
+					Params: json.RawMessage(`[]`),
+				},
+			},
+			klaytnLogResponse{},
+			true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := klaytnLogRequestToResponse(tt.args.msg)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("klaytnLogRequestToResponse() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("klaytnLogRequestToResponse() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
 func Test_handleKlaytnBlockNumber(t *testing.T) {
 	type args struct {
 		msg JsonrpcMessage
@@ -34,13 +113,66 @@ func Test_handleKlaytnBlockNumber(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			got, ok := GetCannedResponse("klay", tt.args.msg)
+			got, ok := GetCannedResponse("klaytn", tt.args.msg)
 			if ok != tt.wantOk {
 				t.Errorf("handleKlaytnBlockNumber() ok = %v, wantOk %v", ok, tt.wantOk)
 				return
 			}
 			if !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("handleKlaytnBlockNumber() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_handleKlaytnGetLogs(t *testing.T) {
+	type args struct {
+		msg JsonrpcMessage
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    []JsonrpcMessage
+		wantErr bool
+	}{
+		{
+			"returns a response with the correct ID",
+			args{
+				JsonrpcMessage{
+					ID:     []byte(`123`),
+					Params: json.RawMessage(fmt.Sprintf(`[{"topics":[["%s"]],"address":["%s"]}]`, hash.String(), address.String())),
+				},
+			},
+			[]JsonrpcMessage{
+				{
+					Version: "2.0",
+					ID:      []byte(`123`),
+					Result:  interfaceToJson([]klaytnLogResponse{getKlaytnLogResponse(address.String(), []string{hash.String()})}),
+				},
+			},
+			false,
+		},
+		{
+			"fails on missing address",
+			args{
+				JsonrpcMessage{
+					ID:     []byte(`123`),
+					Params: json.RawMessage(fmt.Sprintf(`[{"topics":[["%s"]],"address":[]}]`, hash.String())),
+				},
+			},
+			nil,
+			true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := handleKlaytnGetLogs(tt.args.msg)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("handleKlaytnGetLogs() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("handleKlaytnGetLogs() got = %v, want %v", got, tt.want)
 			}
 		})
 	}
@@ -74,13 +206,13 @@ func Test_handleKlaytnRequest(t *testing.T) {
 				{
 					Version: "2.0",
 					Method:  "klay_subscribe",
-					Params:  json.RawMessage(fmt.Sprintf(`{"subscription":"test","result":%s}`, interfaceToJson(getEthLogResponse(address.String(), nil)))),
+					Params:  json.RawMessage(fmt.Sprintf(`{"subscription":"test","result":%s}`, interfaceToJson(getKlaytnLogResponse(address.String(), nil)))),
 				},
 			},
 			false,
 		},
 		{
-			"fails eth_subscribe on RPC",
+			"fails klay_subscribe on RPC",
 			args{
 				"rpc",
 				JsonrpcMessage{
@@ -103,7 +235,7 @@ func Test_handleKlaytnRequest(t *testing.T) {
 			[]JsonrpcMessage{
 				{
 					Version: "2.0",
-					Result:  interfaceToJson([]ethLogResponse{getEthLogResponse(address.String(), []string{hash.String()})}),
+					Result:  interfaceToJson([]klaytnLogResponse{getKlaytnLogResponse(address.String(), []string{hash.String()})}),
 				},
 			},
 			false,
@@ -149,7 +281,7 @@ func Test_handleKlaytnSubscribe(t *testing.T) {
 				{
 					Version: "2.0",
 					Method:  "klay_subscribe",
-					Params:  json.RawMessage(fmt.Sprintf(`{"subscription":"test","result":%s}`, interfaceToJson(getEthLogResponse(address.String(), nil)))),
+					Params:  json.RawMessage(fmt.Sprintf(`{"subscription":"test","result":%s}`, interfaceToJson(getKlaytnLogResponse(address.String(), nil)))),
 				},
 			},
 			false,
@@ -186,6 +318,85 @@ func Test_handleKlaytnSubscribe(t *testing.T) {
 			}
 			if !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("handleKlaytnSubscribe() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_handleKlaytnMapStringInterface(t *testing.T) {
+	type args struct {
+		in map[string]json.RawMessage
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    klaytnLogResponse
+		wantErr bool
+	}{
+		{
+			"correct klay_log request",
+			args{
+				map[string]json.RawMessage{
+					"topics":  json.RawMessage(fmt.Sprintf(`[["%s"]]`, hash.String())),
+					"address": json.RawMessage(fmt.Sprintf(`["%s"]`, address.String())),
+				},
+			},
+			getKlaytnLogResponse(address.String(), []string{hash.String()}),
+			false,
+		},
+		{
+			"klay_log request with empty topics",
+			args{
+				map[string]json.RawMessage{
+					"topics":  json.RawMessage(`[]`),
+					"address": json.RawMessage(fmt.Sprintf(`["%s"]`, address.String())),
+				},
+			},
+			getKlaytnLogResponse(address.String(), nil),
+			false,
+		},
+		{
+			"klay_log request with no topics",
+			args{
+				map[string]json.RawMessage{
+					"address": json.RawMessage(fmt.Sprintf(`["%s"]`, address.String())),
+				},
+			},
+			klaytnLogResponse{},
+			true,
+		},
+		{
+			"uses first address",
+			args{
+				map[string]json.RawMessage{
+					"topics":  json.RawMessage(fmt.Sprintf(`[["%s"]]`, hash.String())),
+					"address": json.RawMessage(fmt.Sprintf(`["%s", "%s"]`, address.String(), address2.String())),
+				},
+			},
+			getKlaytnLogResponse(address.String(), []string{hash.String()}),
+			false,
+		},
+		{
+			"fails on no addresses",
+			args{
+				map[string]json.RawMessage{
+					"topics":  json.RawMessage(fmt.Sprintf(`[["%s"]]`, hash.String())),
+					"address": json.RawMessage(`[]`),
+				},
+			},
+			klaytnLogResponse{},
+			true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := handleKlaytnMapStringInterface(tt.args.in)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("handleKlaytnMapStringInterface() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("handleKlaytnMapStringInterface() got = %v, want %v", got, tt.want)
 			}
 		})
 	}
