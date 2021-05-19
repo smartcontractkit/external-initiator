5f8c221e40db0c8ba22163545c49b4caaab45b04
aalu1418 <aalu1418@gmail.com>
2021-05-19 12:06:42 -0400

    updating subscriber_test file

--- subscriber/subscriber_test.go
+++ subscriber/subscriber_test.go
@@ -1,6 +1,7 @@
 package subscriber
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -11,7 +12,11 @@ import (
 
 	"github.com/gorilla/websocket"
 	"github.com/smartcontractkit/external-initiator/eitest"
+	"github.com/smartcontractkit/external-initiator/store"
 )
+
+var rpcMockUrl *url.URL
+var wsMockUrl *url.URL
 
 func TestMain(m *testing.M) {
 	responses := make(map[string]int)
@@ -27,6 +32,12 @@ func TestMain(m *testing.M) {
 		_, _ = w.Write([]byte(fmt.Sprint(responses[r.URL.Path])))
 	}))
 	defer ts.Close()
+
+	u, err := url.Parse(ts.URL)
+	rpcMockUrl = u
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	ws := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		var c *websocket.Conn
@@ -69,7 +80,7 @@ func TestMain(m *testing.M) {
 	}))
 	defer ws.Close()
 
-	wsMockUrl, err := url.Parse(ws.URL)
+	wsMockUrl, err = url.Parse(ws.URL)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -77,4 +88,71 @@ func TestMain(m *testing.M) {
 
 	code := m.Run()
 	os.Exit(code)
+}
+
+func TestNewSubscriber(t *testing.T) {
+	type fields struct {
+		Url string
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		wantErr bool
+	}{
+		{
+			"succeeds with valid RPC endpoint",
+			fields{Url: rpcMockUrl.String()},
+			false,
+		},
+		{
+			"succeeds with valid WS endpoint",
+			fields{Url: wsMockUrl.String()},
+			false,
+		},
+		{
+			"fails with invalid endpoint",
+			fields{Url: "not_real"},
+			true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			endpoint := store.Endpoint{Url: tt.fields.Url, RefreshInt: 10}
+			if _, err := NewSubscriber(endpoint); (err != nil) != tt.wantErr {
+				t.Errorf("NewSubscriber() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
+
+func TestNewJsonrpcMessage(t *testing.T) {
+	type fields struct {
+		nonce  uint64
+		method string
+		params json.RawMessage
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		wantErr bool
+	}{
+		{
+			"succeeds with valid parameters",
+			fields{nonce: 1, method: "test", params: []byte("{}")},
+			false,
+		},
+		{
+			"fails with invalid params",
+			fields{nonce: 1, method: "test", params: []byte("fail")},
+			true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			_, err := NewJsonrpcMessage(tt.fields.nonce, tt.fields.method, tt.fields.params)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("NewJsonrpcMessage() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
 }
