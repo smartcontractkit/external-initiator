e72ecfe49f6d1f5027ac6acdec7f10acff5fca05
aalu1418 <aalu1418@gmail.com>
2021-08-27 16:58:36 -0400

    fix missing db migrations

--- store/migrations/migration1623859956/migrate.go
+++ store/migrations/migration1623859956/migrate.go
@@ -1,9 +1,19 @@
 package migration1623859956
 
 import (
+	"encoding/json"
+
 	"github.com/jinzhu/gorm"
 	"github.com/pkg/errors"
 )
+
+type Endpoint struct {
+	gorm.Model
+	Url        string
+	Type       string
+	RefreshInt int
+	Name       string `gorm:"unique;not null"`
+}
 
 type TerraSubscription struct {
 	gorm.Model
@@ -14,16 +24,32 @@ type TerraSubscription struct {
 
 type Subscription struct {
 	gorm.Model
-	ReferenceId       string `gorm:"unique;not null"`
-	Job               string
-	EndpointName      string
-	Terra             TerraSubscription
+	ReferenceId  string `gorm:"unique;not null"`
+	Job          string
+	EndpointName string
+	Terra        TerraSubscription
+}
+
+type JobSpec struct {
+	gorm.Model
+	Job  string `gorm:"unique;not null"`
+	Spec json.RawMessage
 }
 
 func Migrate(tx *gorm.DB) error {
 	err := tx.AutoMigrate(&Subscription{}).Error
 	if err != nil {
 		return errors.Wrap(err, "failed to auto migrate Subscription")
+	}
+
+	err = tx.AutoMigrate(&Endpoint{}).Error
+	if err != nil {
+		return errors.Wrap(err, "failed to auto migrate Endpoint")
+	}
+
+	err = tx.AutoMigrate(&JobSpec{}).Error
+	if err != nil {
+		return errors.Wrap(err, "failed to auto migrate Job Specs")
 	}
 
 	err = tx.AutoMigrate(&TerraSubscription{}).AddForeignKey("subscription_id", "subscriptions(id)", "CASCADE", "CASCADE").Error
